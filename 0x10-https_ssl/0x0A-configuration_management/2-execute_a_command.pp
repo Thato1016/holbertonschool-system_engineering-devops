2df0dcc7fc85e359d882197ae0234546e8aa8b17
Thato1016 <98318441+Thato1016@users.noreply.github.com>
2022-08-25 21:53:30 +0200

    HTTPS SSL

--- 0x10-https_ssl/0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Using Puppet, create a manifest that kills a process named killmenow
-exec { 'killmenow':
-  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command  => 'pkill killmenow',
-  provider => 'shell',
-}
